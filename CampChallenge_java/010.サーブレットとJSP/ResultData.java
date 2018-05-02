8f299032f0b8bbd575719b8ec699ea42fa4cc667
DESKTOP-IRQECFB\guest1Day <yuuta0477@gmail.com>
2018-05-02 10:44:02 +0900

    占いシステムをJSPで表示

---
+++ CampChallenge_java/010.サーブレットとJSP/ResultData.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class ResultData implements Serializable{
+
+    private Date d;
+    private String luck;
+    public ResultData(){}
+
+    public Date getD(){
+        return d;
+    }
+
+    public void setD(Date d){
+        this.d = d;
+    }
+    public String getLuck(){
+        return luck;
+    }
+    public void setLuck(String luck){
+        this.luck = luck;
+    }
+
+}
