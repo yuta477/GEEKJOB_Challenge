5ad3394e61765f801bfbbfd324bbf91499873caf
DESKTOP-IRQECFB\guest1Day <yuuta0477@gmail.com>
2018-05-02 12:35:39 +0900

    クラスの作成

---
+++ CampChallenge_java/013.クラス/ClassUse/Dog.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.ClassUse;
+
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Dog {
+     
+        public String name = "";
+        public int age = 0;
+        
+       //メソッドの宣言
+        public void setdog(String n,int a){
+            //引数をフィールドへ設定
+            this.name = n;
+            this.age = a;
+        }
+        
+        public void print(){
+            String print = "名前は" + this.name + "<br>年齢は" + this. age;
+            System.out.print(print);
+            }
+    }
