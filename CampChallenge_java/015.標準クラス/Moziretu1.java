477d16e125a61c8e46982cd2182f95248426f60f
DESKTOP-IRQECFB\guest1Day <yuuta0477@gmail.com>
2018-05-11 13:39:28 +0900

    バイト数と文字数の取得

---
+++ CampChallenge_java/015.標準クラス/Moziretu1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.String;
+
+/**
+ *
+ * @author yuta
+ */
+public class Moziretu1 {
+    public static void main(String[] args) {
+        String name = "さいとうゆうた";
+
+        // 文字列の長さを表示 -- 5
+        System.out.println(name.length());
+        System.out.println(name.getBytes().length);
+    }
+}
+   
+
