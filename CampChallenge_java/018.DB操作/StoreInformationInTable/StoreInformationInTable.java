7c878b7c6d47f9a7326469cb2343722079e4747a
DESKTOP-IRQECFB\guest1Day <yuuta0477@gmail.com>
2018-05-28 10:11:29 +0900

    テーブルへ情報を格納

---
+++ CampChallenge_java/018.DB操作/StoreInformationInTable/StoreInformationInTable.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.StoreInformationInTable;
+
+import java.sql.*;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class StoreInformationInTable {
+    public static void main(String[] args){
+     Connection db_con = null;
+     PreparedStatement db_st = null;
+     ResultSet db_data = null;
+
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            db_con = DriverManager.getConnection("jdbc:mysql://localhost:3306/geekjob_db","yuta","19920707");
+            
+            
+            db_st = db_con.prepareStatement ("INSERT INTO profiles(profilesID,name,tel,age,birthday)VALUES(3,'田中 太郎','0120-7777-7777',20,'2000-7-7')");
+                    
+//            検索        
+//            db_st = db_con.prepareStatement("select * from profiles where name = ? OR age=?");
+//            db_st.setString(1, "田中 実");
+//            db_st.setInt(2,30);
+            
+            db_st.executeUpdate();
+
+
+//            while(db_data.next()){
+//                System.out.print("名前：" + db_data.getString("name") + "<br>");
+//            }
+            
+            db_con.close();
+            db_st.close();
+            db_con.close();
+            
+        } catch (SQLException e_sql) {
+            System.out.println("接続時にエラーが発生しました:"+e_sql.toString());
+        } catch (Exception e) {
+            System.out.println("接続時にエラーが発生しました:"+e.toString());
+        } finally {
+            if (db_con != null) {
+                try {
+                    db_con.close();
+                } catch (Exception e_con) {
+                    System.out.println(e_con.getMessage());
+                }
+            }
+        }    
+    }
+}
+
